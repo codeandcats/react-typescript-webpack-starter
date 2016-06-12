9ca14e5becfd6e961a9f0c6220645fd40be932f2
Ben <codeandcats@gmail.com>
2016-06-12 13:07:31 +1000

    Updated paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: "./typescripts/app/index.tsx",
     output: {
-        filename: "./dist/bundle.js",
+        filename: "./bundle.js",
     },
 
     // Enable sourcemaps for debugging webpack's output.
